ca824629a2d21313c36fee1eb84c9dd8bf3367c9
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-05-22 15:35:31 -0300

    feat: improve DTO with partial types

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 // O que é o DTO: Data Transfer Object (objeto de transferência de dados) é um objeto que contém apenas os dados necessários para realizar uma operação.
 
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString({ message: 'O nome deve ser uma string' })
+  @MinLength(5, { message: 'O nome deve ter no mínimo 5 caracteres' })
+  @MaxLength(30, { message: 'O nome não pode ter mais de 30 caracteres' })
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
   readonly name: string;
+
+  @IsString({ message: 'A descrição deve ser uma string' })
+  @MinLength(10, { message: 'A descrição deve ter no mínimo 10 caracteres' })
+  @MaxLength(100, {
+    message: 'A descrição não pode ter mais de 100 caracteres',
+  })
+  @IsNotEmpty({ message: 'A descrição é obrigatória' })
   readonly description: string;
 }
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,5 +1,29 @@
-export class UpdateTaskDto {
-  readonly name?: string;
-  readonly description?: string;
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsBoolean, IsOptional } from 'class-validator';
+
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTaskDto } from './create-task.dto';
+
+// export class UpdateTaskDto {
+//   @IsString()
+//   @IsOptional()
+//   @IsNotEmpty()
+//   readonly name?: string;
+
+//   @IsString()
+//   @IsOptional()
+//   @IsNotEmpty()
+//   readonly description?: string;
+
+//   @IsBoolean()
+//   @IsOptional()
+//   readonly completed?: boolean;
+// }
+
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsBoolean()
+  @IsOptional()
   readonly completed?: boolean;
 }
+
+// Para não precisar de um DTO separado, podemos usar o @PartialType do @nestjs/mapped-types para criar um DTO baseado em outro. Isso permite que você crie um DTO que herda de outro DTO e adicione ou remova propriedades. No entanto, é importante notar que o @PartialType não é uma validação, ele apenas cria um novo DTO com as propriedades que você especificou. Para validar os dados, você precisa usar a validação de propriedades do TypeScript.
