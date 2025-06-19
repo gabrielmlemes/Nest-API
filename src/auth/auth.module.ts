53262f7e505e2e3f18c89da075f8dca6e0f08d37
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-06-19 13:55:00 -0300

    feat(jwt): create initial jwt config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { HashingServiceProtocol } from './hash/hashing.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
 
 @Global() // Pode ser usado em qualquer lugar do app, não precisa importar em nenhum outro módulo além do app module
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ConfigModule.forFeature(jwtConfig)],
   providers: [
     {
       provide: HashingServiceProtocol,
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,18 @@
 import { LoginDto } from './dto/loginDto';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HashingServiceProtocol } from './hash/hashing.service';
+import jwtConfig from './config/jwt.config';
+import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
     private prisma: PrismaService,
     private readonly hashingService: HashingServiceProtocol,
+
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
   ) {}
 
   async login(LoginDto: LoginDto) {
